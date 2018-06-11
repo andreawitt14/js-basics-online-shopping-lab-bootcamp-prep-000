ce05a7412a34d23b89044b8d4645ebcd46c931c3
Andrea Witt <andreawitt14@hotmail.com>
2018-06-11 16:37:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,16 +51,9 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   
-  for(let i = 0; i < cart.length; i++){
-    if(item === cart[i].itemName){
-      cart.splice(i, 1)
-      return cart;
-    }
-    else{
-      return "That item is not in your cart.";
-    }
+  
     
-  }
+  
 }
 
 function placeOrder(cardNumber) {
